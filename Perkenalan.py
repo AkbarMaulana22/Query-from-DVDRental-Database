adf157ad794790a2db7622818205e87e22420c13
AkbarMaulana22 <42316475+AkbarMaulana22@users.noreply.github.com>
2020-04-20 12:40:06 +0700

    Add files via upload

---
+++ Perkenalan.py
@@ -0,0 +1,6 @@
+def perkenalan():
+    print("Please fill in your details and fill in English")
+    Name=input("Input Your Name: ")
+    Address=input("Input Your Address: ")
+    DOB=input("Input Your Date of Birth: ")
+    print("My name is "+str(Name)+", "+"I live in "+str(Address)+", "+"and I was born on "+str(DOB))
